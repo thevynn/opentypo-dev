7f43e0bf4317ec8a1581b0a69d4e35853374425f
thevynn <163614291+thevynn@users.noreply.github.com>
2024-08-24 05:57:55 +0000

    MVP Deploy: reCAPTCHA Update

--- opentypo/src/app/[locale]/(main)/layout.tsx
+++ opentypo/src/app/[locale]/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import "@/styles/globals.css";
 
 import { Analytics } from "@vercel/analytics/react";
@@ -21,9 +22,43 @@ const pretendard = localFont({
   variable: "--font-pretendard",
 });
 
-export const metadata: Metadata = {
-  title: "OpenTypo",
-  description: "Free, but Better.",
+export const metadata = {
+  title: "OpenTypo™",
+  description: "유료보다 예쁜 상업적 무료 폰트를 큐레이팅 해드립니다.",
+  generator: "Next.js",
+  applicationName: "OpenTypo™",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "폰트",
+    "글꼴",
+    "무료폰트",
+    "상업적무료",
+    "오픈폰트",
+    "타이포그래피",
+    "무료폰트 모음",
+    "fonts",
+    "typography",
+    "free fonts",
+    "open fonts",
+    "예쁜폰트",
+    "디자인 폰트",
+    "디자이너",
+    "마케터",
+    "PPT",
+    "마케팅",
+    "그래픽",
+  ],
+  authors: [{ name: "thevynn" }, { name: "Design eXperience™" }],
+  creator: "thevynn",
+  publisher: "Design eXperience™",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  icons: {
+    icon: "/images/fav.png",
+  },
 };
 
 export default async function LocaleLayout({
